215369f0ed73cf6050f5072943a9d9d9e83ef6c7
Dave Syer <dsyer@pivotal.io>
2014-09-23 08:36:07 +0100

    Add Hystrix layer (not working yet)

--- src/main/java/demo/RecommendationApplication.java
+++ src/main/java/demo/RecommendationApplication.java
@@ -1,11 +1,12 @@
 package demo;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
@@ -20,7 +22,6 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.client.Traverson;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFutureTask;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +29,11 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 import reactor.core.Reactor;
 import rx.Observable;
-import rx.subscriptions.Subscriptions;
+import rx.Subscriber;
 
 import com.netflix.discovery.DiscoveryClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.command.ObservableResult;
 
 import demo.StoreDetails.Recommendation;
 import demo.lifecycle.EnableLifecycle;
@@ -41,6 +44,7 @@ import demo.lifecycle.Start;
 @EnableAutoConfiguration
 @EnableLifecycle
 @EnableEurekaClient
+@EnableHystrix
 @RestController
 public class RecommendationApplication {
 
@@ -48,12 +52,12 @@ public class RecommendationApplication {
 	private Reactor reactor;
 
 	@Autowired
-	private RecommendationService service;
+	private StoreService stores;
 
 	@RequestMapping("/{customerId}")
 	public DeferredResult<List<StoreDetails>> recommend(@PathVariable String customerId)
 			throws Exception {
-		return toDeferredResult(service.fetch(customerId));
+		return toDeferredResult(fetch(customerId));
 	}
 
 	@Start
@@ -62,6 +66,22 @@ public class RecommendationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecommendationApplication.class, args);
+	}
+
+	private Observable<StoreDetails> fetch(String customerId) {
+		Observable<StoreDetails> details = stores.nearbyStores(customerId)
+				.<StoreDetails> flatMap(
+						store -> {
+							Observable<Recommendation> recommendations = stores
+									.recommendationsForStore(store.getId());
+							return recommendations.buffer(500, TimeUnit.MILLISECONDS, 10)
+									.first().map(list -> {
+										StoreDetails result = new StoreDetails(store);
+										result.getRecommendations().addAll(list);
+										return result;
+									});
+						});
+		return details;
 	}
 
 	public static <T> DeferredResult<List<T>> toDeferredResult(Observable<T> observable) {
@@ -74,7 +94,7 @@ public class RecommendationApplication {
 
 	public static <S extends Future<C>, C extends Collection<T>, T> Observable<T> collectionToObservable(
 			S collection) {
-		return Observable.create(observer -> {
+		return Observable.create((Subscriber<? super T> observer) -> {
 			try {
 				for (T item : collection.get()) {
 					observer.onNext(item);
@@ -84,7 +104,6 @@ public class RecommendationApplication {
 			catch (Exception e) {
 				observer.onError(e);
 			}
-			return Subscriptions.empty();
 		});
 
 	}
@@ -92,58 +111,99 @@ public class RecommendationApplication {
 }
 
 @Component
-class RecommendationService {
+class StoreService {
 
-	private DiscoveryClient client;
+	private LameService lame;
 
 	@Autowired
-	public RecommendationService(DiscoveryClient client) {
-		this.client = client;
+	public StoreService(LameService lame) {
+		this.lame = lame;
 	}
 
-	public Observable<StoreDetails> fetch(String customerId) {
-		Observable<StoreDetails> details = nearbyStores(customerId)
-				.<StoreDetails> flatMap(
-						store -> {
-							Observable<Recommendation> recommendations = RecommendationApplication
-									.collectionToObservable(recommendations(store));
-							return recommendations.buffer(500, TimeUnit.MILLISECONDS, 10)
-									.first().map(list -> {
-										StoreDetails result = new StoreDetails(store);
-										result.getRecommendations().addAll(list);
-										return result;
-									});
-						});
-		return details;
+	public Observable<Recommendation> recommendationsForStore(String storeId) {
+		// TODO: iterate over pageable results from rest template
+		return Observable.create((Subscriber<? super Recommendation> observer) -> {
+			lame.recommendationsForStore(storeId).subscribe((list) -> {
+				for (Recommendation recommendation : list) {
+					observer.onNext(recommendation);
+				}
+			}, (e) -> {
+				observer.onError(e);
+			}, () -> {
+				observer.onCompleted();
+			});
+		});
 	}
 
 	public Observable<Store> nearbyStores(String customerId) {
-		return RecommendationApplication.collectionToObservable(stores(customerId));
+		// TODO: iterate over pageable results from rest template
+		return Observable.create((Subscriber<? super Store> observer) -> {
+			lame.nearbyStores(customerId).subscribe((list) -> {
+				for (Store recommendation : list) {
+					observer.onNext(recommendation);
+				}
+			}, (e) -> {
+				observer.onError(e);
+			}, () -> {
+				observer.onCompleted();
+			});
+		});
 	}
 
-	private Future<Collection<Store>> stores(String customerId) {
+}
+
+@Component
+class LameService {
+
+	private final DiscoveryClient client;
+
+	@Autowired
+	public LameService(DiscoveryClient client) {
+		this.client = client;
+	}
+
+	@HystrixCommand(fallbackMethod = "emptyStream")
+	public Observable<Collection<Recommendation>> recommendationsForStore(String storeId) {
+		return new ObservableResult<Collection<Recommendation>>() {
+			@Override
+			public Collection<Recommendation> invoke() {
+				return recommendations(storeId);
+			}
+		};
+	}
+
+	@HystrixCommand(fallbackMethod = "emptyStream")
+	public Observable<Collection<Store>> nearbyStores(String customerId) {
+		return new ObservableResult<Collection<Store>>() {
+			@Override
+			public Collection<Store> invoke() {
+				return stores(customerId);
+			}
+		};
+	}
+
+	protected <T> Collection<T> emptyStream(String id) {
+		return Collections.emptyList();
+	}
+
+	private Collection<Store> stores(String customerId) {
 		String url = client.getNextServerFromEureka("CUSTOMERS", false).getHomePageUrl();
-		final ListenableFutureTask<Collection<Store>> future = new ListenableFutureTask<Collection<Store>>(
-				() -> {
-					final Collection<Store> stores = new Traverson(new URI(url + "customers/" + customerId),
-							MediaTypes.HAL_JSON).follow("stores-nearby").toObject(
-							new ParameterizedTypeReference<Resources<Store>>() {
-							}).getContent();
-					return stores;
-				});
-		// We need to get the result populated somehow
-		Executors.newSingleThreadExecutor().submit(future);
-		return future;
+		URI base;
+		try {
+			base = new URI(url + "customers/" + customerId);
+		}
+		catch (URISyntaxException e) {
+			throw new IllegalStateException("Could not create base URI", e);
+		}
+		final Collection<Store> stores = new Traverson(base, MediaTypes.HAL_JSON)
+				.follow("stores-nearby")
+				.toObject(new ParameterizedTypeReference<Resources<Store>>() {
+				}).getContent();
+		return stores;
 	}
 
-	private Future<List<Recommendation>> recommendations(Store store) {
-		ListenableFutureTask<List<Recommendation>> future = new ListenableFutureTask<List<Recommendation>>(
-				() -> {
-					return Arrays.asList(new Recommendation(store.getId()));
-				});
-		// We need to get the result populated somehow
-		Executors.newSingleThreadExecutor().submit(future);
-		return future;
+	private List<Recommendation> recommendations(String store) {
+		return Arrays.asList(new Recommendation(store));
 	}
 
 }
