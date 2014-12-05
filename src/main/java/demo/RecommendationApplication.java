d9c5c00442a90dd30afc3aba2f81aea6fbf9bf54
Dave Syer <dsyer@pivotal.io>
2014-12-05 17:51:16 +0000

    Update to latest netflix bits

--- src/main/java/demo/RecommendationApplication.java
+++ src/main/java/demo/RecommendationApplication.java
@@ -16,8 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
@@ -42,8 +42,8 @@ import demo.StoreDetails.Recommendation;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableEurekaClient
-@EnableHystrix
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 @RestController
 public class RecommendationApplication {
 
